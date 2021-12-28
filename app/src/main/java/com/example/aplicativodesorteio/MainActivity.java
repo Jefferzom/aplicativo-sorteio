74a75d65bdd7e944339cf11b2ae36c582a411205
Jefferson <jefferson.toledo@fengbrasil.com.br>
2021-12-27 23:12:35 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/aplicativodesorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.aplicativodesorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void jogar() {
+        TextView textResultado;
+
+        textResultado = findViewById(R.id.textResultado);
+
+
+    }
+}
